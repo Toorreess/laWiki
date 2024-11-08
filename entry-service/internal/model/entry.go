f4b0df8cc2e346e0815d1bda27f9ae9d17107176
Toorreess <jostorpos@uma.es>
2024-11-08 19:14:59 +0100

    🔧 refactor: deleted field added

--- entry-service/internal/model/entry.go
+++ entry-service/internal/model/entry.go
@@ -1,10 +1,18 @@
 package model
 
+import "time"
+
 type Entry struct {
 	ID      string `json:"id" firestore:"-"`
 	Name    string `json:"name" firestore:"name"`
 	Author  string `json:"author" firestore:"author"`
 	Content string `json:"content" firestore:"content"`
 	WikiID  string `json:"wiki_id" firestore:"wiki_id"`
-	Deleted bool   `json:"-" firestore:"deleted"`
+
+	// Deleted is used for logical deletion
+	Deleted bool `json:"-" firestore:"deleted"`
+
+	// Firestore document metadata
+	CreationDate     time.Time `json:"creation_date" firestore:"-"`
+	ModificationDate time.Time `json:"modification_date" firestore:"-"`
 }
